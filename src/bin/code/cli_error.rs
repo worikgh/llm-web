642cde322bd0361045594aafd355a85ba7c0624e
Worik <5387413+worikgh@users.noreply.github.com>
2023-05-26 09:46:26 +1200

    Comment out code

--- src/bin/code/cli_error.rs
+++ src/bin/code/cli_error.rs
@@ -1,20 +1,20 @@
-use std::{error::Error, fmt};
+// use std::{error::Error, fmt};
 
-#[derive(Debug)]
-pub enum CliError {
-    Error(String),
-}
-impl CliError {}
-impl fmt::Display for CliError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            CliError::Error(ref msg) => write!(f, "Error: {msg}"),
-        }
-    }
-}
+// #[derive(Debug)]
+// pub enum CliError {
+//     Error(String),
+// }
+// impl CliError {}
+// impl fmt::Display for CliError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         match self {
+//             CliError::Error(ref msg) => write!(f, "Error: {msg}"),
+//         }
+//     }
+// }
 
-impl Error for CliError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        None
-    }
-}
+// impl Error for CliError {
+//     fn source(&self) -> Option<&(dyn Error + 'static)> {
+//         None
+//     }
+// }
