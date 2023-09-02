091a71ac448a4534c3766bc52e7eccf949c0653a
Worik <5387413+worikgh@users.noreply.github.com>
2023-09-03 11:07:40 +1200

    Update display

--- llm-web-fe/src/lib.rs
+++ llm-web-fe/src/lib.rs
@@ -1,10 +1,15 @@
 // use chat_div::chat_div;
 use login_div::login_div;
+use set_page::initialise_page;
 use set_page::set_page;
 use wasm_bindgen::prelude::*;
-use web_sys::{window, HtmlLinkElement};
-// mod chat_div;
+// use web_sys::{window, HtmlLinkElement};
+#[allow(unused_imports)]
+use crate::utility::print_to_console;
+mod chat_div;
+mod cost_div;
 mod login_div;
+mod make_request;
 mod manipulate_css;
 mod set_page;
 mod utility;
@@ -27,28 +32,11 @@ mod utility;
 // }
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
+    initialise_page()?;
     start_app()
 }
 
 fn start_app() -> Result<(), JsValue> {
-    let document = window()
-        .and_then(|win| win.document())
-        .expect("Failed to get document");
-
-    // Style
-    let link: HtmlLinkElement = document.create_element("link").unwrap().dyn_into().unwrap();
-    link.set_rel("stylesheet");
-    link.set_href("/style.css");
-    document.head().unwrap().append_child(&link).unwrap();
-
-    let body = document.body().expect("Could not access document.body");
-
     set_page(login_div)?;
-
     Ok(())
-}
-
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
 }
