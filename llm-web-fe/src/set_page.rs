ec3810b037b172f7e3aae18af682b3c47c700ec7
Worik <5387413+worikgh@users.noreply.github.com>
2023-09-03 11:04:02 +1200

    InitialisePage

--- llm-web-fe/src/set_page.rs
+++ llm-web-fe/src/set_page.rs
@@ -1,3 +1,4 @@
+use crate::manipulate_css::add_css_rule;
 #[allow(unused_imports)]
 use crate::utility::print_to_console;
 #[allow(unused_imports)]
@@ -5,30 +6,43 @@ use crate::utility::print_to_console_s;
 use wasm_bindgen::prelude::*;
 use web_sys::window;
 use web_sys::{Document, Element, HtmlElement};
-const MAIN_DIV_NAME: &str = "llm-rs";
+const MAIN_DIV_NAME: &str = "main_body";
 
-/// `set_page(f, c)`: Display a page.  `f` is the function that builds
+/// `set_page(f)`: Display a page.  `f` is the function that builds
 /// the page to display.
+/// Structure of the page
 pub fn set_page(f: impl Fn(&Document) -> Result<Element, JsValue>) -> Result<(), JsValue> {
-    print_to_console("set_page 1");
     // Get the main document
     let document = window()
         .and_then(|win| win.document())
         .expect("Failed to get document");
     let body = document.body().expect("Could not access document.body");
-
     let e = f(&document)?;
-    if let Some(old_div) = document.get_element_by_id(MAIN_DIV_NAME) {
-        body.remove_child(&old_div)?;
-    }
 
-    let new_main_div = document.create_element("DIV").unwrap();
-    new_main_div.set_attribute("id", MAIN_DIV_NAME).unwrap();
-    new_main_div.append_child(&e)?;
-    body.append_child(&new_main_div)?;
+    if let Some(main_body) = document.get_element_by_id(MAIN_DIV_NAME) {
+        main_body.set_inner_html("");
+        main_body.append_child(&e)?;
+        body.append_child(&main_body)?;
+        print_to_console("Setting prompt-input as focus");
+        set_focus_on_element(&document, "prompt-input");
+        print_to_console("Setted prompt-input as focus");
+    } else {
+        print_to_console("No `main_body` in page.  Has not been initialised");
+        panic!("Died");
+    }
     Ok(())
 }
 
+#[allow(dead_code)]
+pub fn set_status(document: &Document, status: &str) {
+    if let Some(status_element) = document.get_element_by_id("status-div") {
+        status_element.set_inner_html(status);
+    } else {
+        print_to_console_s(format!("Status (No status-div): {status}"));
+    }
+}
+
+#[allow(dead_code)]
 pub fn set_focus_on_element(document: &Document, element_id: &str) {
     // let document = web_sys::window().unwrap().document().unwrap();
     if let Some(element) = document.get_element_by_id(element_id) {
@@ -44,4 +58,82 @@ pub fn set_focus_on_element(document: &Document, element_id: &str) {
             "Failed to set focus.  Could not find: {element_id}"
         ));
     }
+}
+
+/// Set up the basic page with header, footer, and body
+#[allow(dead_code)]
+pub fn initialise_page() -> Result<(), JsValue> {
+    print_to_console("initialise_page()");
+    let document = window()
+        .and_then(|win| win.document())
+        .expect("Failed to get document");
+    let body = document.body().expect("Could not access document.body");
+    while let Some(child) = body.first_child() {
+        let _ = body.remove_child(&child);
+    }
+
+    // Set up the three divs
+    let footer_div = document.create_element("div")?;
+    footer_div.set_id("footer");
+    let header_div = document.create_element("div")?;
+    header_div.set_id("header");
+    let body_div = document.create_element("div")?;
+    body_div.set_id("main_body");
+
+    // Add the divs
+    body.append_child(&footer_div)?;
+    body.append_child(&header_div)?;
+    body.append_child(&body_div)?;
+
+    // The style.  Sies given in integer units of percent
+    let footer_height = 10;
+    let header_height = 10;
+    let main_body_height = 100 - (footer_height + header_height);
+    let main_width = 100;
+    add_css_rule(&document, "html, body", "height", "100%")?;
+    add_css_rule(&document, "html, body", "margin", "0")?;
+    add_css_rule(&document, "html, body", "padding", "0")?;
+    add_css_rule(&document, "#header", "height", format!("{header_height}%"))?;
+    add_css_rule(&document, "#header", "width", format!("{main_width}%"))?;
+    add_css_rule(&document, "#header", "position", "fixed")?;
+    add_css_rule(&document, "#header", "top", "0")?;
+    add_css_rule(&document, "#header", "left", "0")?;
+    add_css_rule(&document, "#sidepanel", "height", "100%")?;
+    add_css_rule(&document, "#sidepanel", "width", "0%")?;
+    add_css_rule(&document, "#sidepanel", "position", "fixed")?;
+    add_css_rule(&document, "#sidepanel", "top", "0")?;
+    add_css_rule(&document, "#sidepanel", "left", "0")?;
+    add_css_rule(&document, "#footer", "height", format!("{footer_height}%"))?;
+    add_css_rule(&document, "#footer", "width", format!("{main_width}%"))?;
+    add_css_rule(&document, "#footer", "position", "fixed")?;
+    add_css_rule(
+        &document,
+        "#footer",
+        "top",
+        format!("{}%", 100 - footer_height),
+    )?;
+    add_css_rule(&document, "#footer", "left", "0%")?;
+
+    add_css_rule(
+        &document,
+        "#main_body",
+        "height",
+        format!("{main_body_height}%"),
+    )?;
+    add_css_rule(&document, "#main_body", "width", format!("{main_width}%"))?;
+    add_css_rule(&document, "#main_body", "position", "fixed")?;
+    add_css_rule(&document, "#main_body", "top", format!("{header_height}%"))?;
+    add_css_rule(&document, "#main_body", "left", "0%")?;
+    add_css_rule(
+        &document,
+        "#main_body",
+        "bottom",
+        format!("{}%", 100 - footer_height),
+    )?;
+    add_css_rule(&document, "#sidepanel", "border", "1px solid black")?;
+    add_css_rule(&document, "#footer", "border", "1px solid black")?;
+    add_css_rule(&document, "#header", "border", "1px solid black")?;
+    add_css_rule(&document, "#main_body", "border", "1px solid black")?;
+
+    Ok(())
 }
