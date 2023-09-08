2cda24dee07d50ada08fa091b912b2d537f7e7e2
Worik <5387413+worikgh@users.noreply.github.com>
2023-09-09 09:02:23 +1200

    Initial

---
+++ llm-web-fe/src/filters.rs
@@ -0,0 +1,16 @@
+/// Filter HTML text so it is displayed properly.
+pub fn filter_html(input: &str) -> String {
+    let mut result = String::new();
+    for c in input.chars() {
+        match c {
+            '\n' => result.push_str("<br/>"),
+            '<' => result.push_str("&lt;"),
+            '>' => result.push_str("&gt;"),
+            '&' => result.push_str("&amp;"),
+            '"' => result.push_str("&quot;"),
+            '\'' => result.push_str("&#39;"),
+            _ => result.push(c),
+        }
+    }
+    result
+}
