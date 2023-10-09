e22868a9d38b3fb211484dc4da0f13f8624811d9
Worik <5387413+worikgh@users.noreply.github.com>
2023-10-09 18:55:02 +1300

    Disable build.rs

--- llm-web-fe/build.rs
+++ llm-web-fe/build.rs
@@ -1,13 +1,4 @@
-use std::process::Command;
-
 fn main() {
-    // Execute the custom build.sh script
-    let output = Command::new("./build.sh")
-        .output()
-        .expect("Failed to execute build script");
-
-    // Check the exit code of the script
-    if !output.status.success() {
-        panic!("Build script exited with an error");
-    }
+    println!("In build.rs STDOUT");
+    eprintln!("In build.rs STDERR");
 }
